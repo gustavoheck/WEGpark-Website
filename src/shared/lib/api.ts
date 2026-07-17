7442d29ae72c53f07fb1ff7e74d3e49ac66bd97d
DAVID HILLESHEIM <david_hillesheim@estudante.sesisenai.org.br>
2026-07-17 11:06:14 -0300

    refactor: install stack and start project structure

---
+++ src/shared/lib/api.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      // Logout Logic
+    }
+    return Promise.reject(error);
+  }
+);
