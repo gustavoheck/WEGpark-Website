0dab99c4d639882f42298dee913bc00269230f8f
matheus_oliveira167 <matheus_oliveira167@estudante.sesisenai.org>
2026-07-17 08:54:57 -0300

    chore: setup shadcn and base components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/shared/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "WEGpark",
@@ -14,7 +18,7 @@ export default function RootLayout({
   return (
     <html
       lang="pt-BR"
-      className={`h-full antialiased`}
+      className={cn("h-full", "antialiased", "font-sans", geist.variable)}
     >
       <body>{children}</body>
     </html>
