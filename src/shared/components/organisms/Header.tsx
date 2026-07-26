e0cee3da0f43d7fdb593ad2d363d4e44aac49706
OsuehtaM <matheusdeoliveira042008@gmail.com>
2026-07-25 23:20:11 -0300

    refactor: adjusted header for mobile user

--- src/shared/components/organisms/Header.tsx
+++ src/shared/components/organisms/Header.tsx
@@ -2,19 +2,25 @@ import { SidebarTrigger } from "../../../components/ui/sidebar";
 import { Button } from "@/components/ui/button"
 import { Bell } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
     return (
-        <div className="bg-primary flex w-full h-18 items-center justify-between"
+        <div className="bg-primary flex w-full h-18 items-center justify-between px-2"
         >
             <SidebarTrigger
-                variant="ghost"
-                className="h-12 w-12 text-white hover:bg-white/10"
+                variant="none"
+                className="h-12 w-12 text-white"
             />
-            <Image src="/assets/images/logo-white.png" alt="Logo of the WEGpark" height={66} width={98} loading="eager"/>
-            <Button variant="ghost" className="text-white hover:bg-white/10">
-                <Bell className="size-7" />
-            </Button>
+            <Link href="/veiculos">
+                <Image src="/assets/images/logo-white.png" alt="Logo of the WEGpark" height={66} width={98} loading="eager" />
+            </Link>
+            <Link href="/notificacoes" className="flex items-center">
+                <Button variant="none">
+                    <Bell className="h-7 w-7 text-white size-5" />
+                </Button>
+            </Link>
+
         </div>
     )
 }
