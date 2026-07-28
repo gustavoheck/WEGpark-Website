bd59f4aa46e30fc6ab789ab1fa27ed201f48e087
matheus_oliveira167 <matheus_oliveira167@estudante.sesisenai.org>
2026-07-28 09:23:19 -0300

    fix: update page occurrence detail enums to match recent refactor

--- src/features/occurrences/types/BaseWarning.ts
+++ src/features/occurrences/types/BaseWarning.ts
@@ -1,14 +1,9 @@
 import { WarningType } from "../enums/warning-type"
 import { DefaultOccurence } from "./DefaultOccurence"
 
-export interface WarningTypeDetails {
-    warningType : WarningType
-}
-
-
 export interface BaseWarning {
     uuid : string
     defaults : DefaultOccurence
-    warningType: WarningTypeDetails
+    warningType: WarningType
     description : string
 }
